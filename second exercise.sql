cb7bbe7474f91560219047adef672f1e782c97a1
KaoDlamini <kaohsiung.dlamini@gmail.com>
2024-05-24 00:30:25 +0200

    SQL Submitted

---
+++ second exercise.sql
@@ -0,0 +1,18 @@
+
+select *
+from actor,film,customer
+
+select title
+from sakila.film
+
+select distinct (language_id) as language
+from film
+
+select count(store_id)
+from store
+
+select count(staff_id)
+from staff
+
+select distinct (first_name) 
+from staff
